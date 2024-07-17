0b9d0d83f4ee8493218d4c368ef03674ddc9c6a6
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2024-07-17 16:43:14 +0200

    Added utilities

---
+++ CompileAndOpenProject.bat
@@ -0,0 +1,3 @@
+dotnet "%programfiles%\Epic Games\UE_5.3\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" AGCEditor Win64 Development -Project="%CD%\AGC.uproject" -WaitMutex -FromMsBuild
+AGC.uproject
+pause
---
+++ DeleteUselessFilesAndFolders.bat
@@ -0,0 +1,6 @@
+rmdir /s /q Binaries
+rmdir /s /q DerivedDataCache
+rmdir /s /q Intermediate
+rmdir /s /q Saved
+rmdir /s /q .vs
+del *.sln
