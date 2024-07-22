1e81d88906618cc93c6d6776ffa10eb48a420cb4
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2024-07-22 13:06:17 +0200

    Improved spaceship movement

--- Plugins/Movement/Source/Movement/Private/SpaceShipMovementComponent.cpp
+++ Plugins/Movement/Source/Movement/Private/SpaceShipMovementComponent.cpp
@@ -2,16 +2,61 @@
 
 USpaceShipMovementComponent::USpaceShipMovementComponent()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void USpaceShipMovementComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    SpaceShipMeshComponent = GetOwner()->GetComponentByClass<UStaticMeshComponent>();
+    ensureMsgf(SpaceShipMeshComponent != nullptr, TEXT("USpaceShipMovementComponent::BeginPlay, Unable to find Static Mesh Component on owning Actor"));
 }
 
 void USpaceShipMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	FVector Movement = FVector::ZeroVector;
-	Movement += CurrentMovementVector.X * DeltaTime * Speed * GetOwner()->GetActorRightVector();
-	Movement += CurrentMovementVector.Y * DeltaTime * Speed * GetOwner()->GetActorForwardVector();
-	
-	GetOwner()->AddActorWorldOffset(Movement);
+    if (CurrentMovementVector != FVector2D::ZeroVector)
+    {
+        FVector Movement = FVector::ZeroVector;
+        Movement += CurrentMovementVector.X * GetOwner()->GetActorRightVector();
+        Movement += CurrentMovementVector.Y * GetOwner()->GetActorForwardVector();
+        Velocity += DeltaTime * Acceleration * Movement;
+
+        if (Velocity.SquaredLength() > MaxSpeed * MaxSpeed)
+            Velocity = Velocity.GetSafeNormal() * MaxSpeed;
+    }
+
+    GetOwner()->AddActorWorldOffset(Velocity);
+
+    UpdateSpaceShipRotation(DeltaTime);
+}
+
+void USpaceShipMovementComponent::UpdateSpaceShipRotation(float DeltaTime)
+{
+    FRotator NextRotation = SpaceShipMeshComponent->GetRelativeRotation();
+
+    if (CurrentMovementVector.X > 0)
+        NextRotation.Roll = FMath::Min(MaxRollAngle, NextRotation.Roll + RollSpeed * DeltaTime);
+    else if (CurrentMovementVector.X < 0)
+        NextRotation.Roll = FMath::Max(-MaxRollAngle, NextRotation.Roll - RollSpeed * DeltaTime);
+    else
+    {
+        if (NextRotation.Roll > 0)
+            NextRotation.Roll = FMath::Max(0, NextRotation.Roll - RollSpeed * DeltaTime);
+        else if (NextRotation.Roll < 0)
+            NextRotation.Roll = FMath::Min(0, NextRotation.Roll + RollSpeed * DeltaTime);
+    }
+    
+    if (CurrentMovementVector.Y > 0)
+        NextRotation.Pitch = FMath::Max(-MaxPitchAngle, NextRotation.Pitch - PitchSpeed * DeltaTime);
+    else if (CurrentMovementVector.Y < 0)
+        NextRotation.Pitch = FMath::Min(MaxPitchAngle, NextRotation.Pitch + PitchSpeed * DeltaTime);
+    else if (NextRotation.Pitch > 0)
+        NextRotation.Pitch = FMath::Max(0, NextRotation.Pitch - PitchSpeed * DeltaTime);
+    else if (NextRotation.Pitch < 0)
+        NextRotation.Pitch = FMath::Min(0, NextRotation.Pitch + PitchSpeed * DeltaTime);
+
+    SpaceShipMeshComponent->SetRelativeRotation(NextRotation);
 }
--- Plugins/Movement/Source/Movement/Public/SpaceShipMovementComponent.h
+++ Plugins/Movement/Source/Movement/Public/SpaceShipMovementComponent.h
@@ -10,15 +10,34 @@ class MOVEMENT_API USpaceShipMovementComponent : public UMovementComponent
 {
     GENERATED_BODY()
 
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float MaxSpeed = 500;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float Acceleration = 1;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float MaxPitchAngle = 15;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float MaxRollAngle = 15;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float PitchSpeed = 50;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float RollSpeed = 50;
+
+    FVector Velocity = FVector::ZeroVector;
     FVector2D CurrentMovementVector = FVector2D::ZeroVector;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-    float Speed = 500;
+    float TimeToReachMaxSpeedTimer = 0;
+
+    UStaticMeshComponent* SpaceShipMeshComponent;
 
 public:
     USpaceShipMovementComponent();
 
+    virtual void BeginPlay() override;
     virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
     void Move(const FVector2D& MovementVector) { CurrentMovementVector = MovementVector; }
+
+private:
+    void UpdateSpaceShipRotation(float DeltaTime);
 };
