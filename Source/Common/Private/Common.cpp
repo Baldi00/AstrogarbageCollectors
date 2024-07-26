32d4b75a2c27e3ec7482f230a8fe886bb758f474
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2024-07-26 14:11:39 +0200

    Added empty common module

---
+++ Source/Common/Private/Common.cpp
@@ -0,0 +1,15 @@
+#include "Common.h"
+
+#define LOCTEXT_NAMESPACE "FCommonModule"
+
+void FCommonModule::StartupModule()
+{
+}
+
+void FCommonModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCommonModule, Common)
