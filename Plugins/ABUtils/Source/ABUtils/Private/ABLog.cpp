2f56bfbc46814ebd9dedc302d3ea8de904fa0e54
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2024-07-30 10:01:47 +0200

    Fixed AB logger

--- Plugins/ABUtils/Source/ABUtils/Private/ABLog.cpp
+++ Plugins/ABUtils/Source/ABUtils/Private/ABLog.cpp
@@ -1,29 +1,29 @@
 #include "ABLog.h"
+#include "Kismet/KismetSystemLibrary.h"
 
-void UABLog::Log(FString String, int32 Line, FString FunctionName)
+void UABLog::Log(UObject* WorldContexObject, FString String, int32 Line, FString FunctionName)
 {
-    int32 Key = rand() % 4294967295 - 2147483648;
-    GEngine->AddOnScreenDebugMessage(Key, 3, FColor::Cyan, String);
-    FString ToPrintLogger = FunctionName + " (Line " + FString::FromInt(Line) + "): " + String;
+    UKismetSystemLibrary::PrintString(WorldContexObject, String, true, false);
+    FString ToPrintLogger = FunctionName + "(" + FString::FromInt(Line) + "): " + String;
     UE_LOG(LogTemp, Warning, TEXT("%s"), *ToPrintLogger);
 }
 
-void UABLog::Log(const char* String, int32 Line, FString FunctionName)
+void UABLog::Log(UObject* WorldContexObject, const char* String, int32 Line, FString FunctionName)
 {
-    Log(FString(String), Line, FunctionName);
+    Log(WorldContexObject, FString(String), Line, FunctionName);
 }
 
-void UABLog::Log(int32 Value, int32 Line, FString FunctionName)
+void UABLog::Log(UObject* WorldContexObject, int32 Value, int32 Line, FString FunctionName)
 {
-    Log(FString::FromInt(Value), Line, FunctionName);
+    Log(WorldContexObject, FString::FromInt(Value), Line, FunctionName);
 }
 
-void UABLog::Log(float Value, int32 Line, FString FunctionName)
+void UABLog::Log(UObject* WorldContexObject, float Value, int32 Line, FString FunctionName)
 {
-    Log(FString::SanitizeFloat(Value), Line, FunctionName);
+    Log(WorldContexObject, FString::SanitizeFloat(Value), Line, FunctionName);
 }
 
-void UABLog::Log(bool bValue, int32 Line, FString FunctionName)
+void UABLog::Log(UObject* WorldContexObject, bool bValue, int32 Line, FString FunctionName)
 {
-    Log(bValue ? FString("true") : FString("false"), Line, FunctionName);
+    Log(WorldContexObject, bValue ? FString("true") : FString("false"), Line, FunctionName);
 }
--- Plugins/ABUtils/Source/ABUtils/Public/ABLog.h
+++ Plugins/ABUtils/Source/ABUtils/Public/ABLog.h
@@ -1,15 +1,15 @@
 #pragma once
 
 #define STRF(...) FString::Printf(L ## __VA_ARGS__)
-#define LOG(ToLog) UABLog::Log(ToLog, __LINE__, FString(__FUNCTION__))
+#define LOG(ToLog) UABLog::Log(this, ToLog, __LINE__, FString(__FUNCTION__))
 #define LOGF(...) LOG(STRF(__VA_ARGS__))
 
 class ABUTILS_API UABLog
 {
 public:
-	static void Log(FString String, int32 Line, FString FunctionName);
-	static void Log(const char* String, int32 Line, FString FunctionName);
-	static void Log(int32 Value, int32 Line, FString FunctionName);
-	static void Log(float Value, int32 Line, FString FunctionName);
-	static void Log(bool bValue, int32 Line, FString FunctionName);
+	static void Log(UObject* WorldContexObject, FString String, int32 Line, FString FunctionName);
+	static void Log(UObject* WorldContexObject, const char* String, int32 Line, FString FunctionName);
+	static void Log(UObject* WorldContexObject, int32 Value, int32 Line, FString FunctionName);
+	static void Log(UObject* WorldContexObject, float Value, int32 Line, FString FunctionName);
+	static void Log(UObject* WorldContexObject, bool bValue, int32 Line, FString FunctionName);
 };
