9ae9a790dcd685c4d9512a371d07ab0991ea7e7f
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2024-08-05 10:21:54 +0200

    Removed sphere collider from checkpoint

--- Source/HittableObjects/Private/Checkpoint.cpp
+++ Source/HittableObjects/Private/Checkpoint.cpp
@@ -1,12 +1,6 @@
 #include "Checkpoint.h"
-#include "Components/SphereComponent.h"
 
 ACheckpoint::ACheckpoint()
 {
     Tags.AddUnique("Checkpoint");
-
-    SphereCollider = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollider"));
-    static FName SphereColliderCollisionProfileName = TEXT("OverlapAll");
-    SphereCollider->SetCollisionProfileName(SphereColliderCollisionProfileName);
-    SphereCollider->SetupAttachment(RootComponent);
 }
--- Source/HittableObjects/Public/Checkpoint.h
+++ Source/HittableObjects/Public/Checkpoint.h
@@ -4,15 +4,10 @@
 #include "HittableObject.h"
 #include "Checkpoint.generated.h"
 
-class USphereComponent;
-
 UCLASS()
 class HITTABLEOBJECTS_API ACheckpoint : public AHittableObject
 {
     GENERATED_BODY()
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Checkpoint", meta = (AllowPrivateAccess = "true"))
-    TObjectPtr<USphereComponent> SphereCollider;
 
 public:
     ACheckpoint();
